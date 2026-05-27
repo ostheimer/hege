725b68f9505bdf1f6642802dc114d7b21cb32df3
Andreas Ostheimer <andreas@ostheimer.at>
2026-05-27 15:29:08 +0200

    fix: limit mobile metro watch folders

--- apps/mobile/metro.config.js
+++ apps/mobile/metro.config.js
@@ -2,11 +2,15 @@ const path = require("node:path");
 const { getDefaultConfig } = require("expo/metro-config");
 
 const config = getDefaultConfig(__dirname);
+const workspaceRoot = path.resolve(__dirname, "../..");
 
-config.watchFolders = [path.resolve(__dirname, "../..")];
-config.resolver.nodeModulesPaths = [
-  path.resolve(__dirname, "node_modules"),
-  path.resolve(__dirname, "../../node_modules")
-];
+const mobileWatchFolders = new Set([
+  path.resolve(workspaceRoot, "node_modules"),
+  path.resolve(__dirname),
+  path.resolve(workspaceRoot, "packages/domain"),
+  path.resolve(workspaceRoot, "packages/tokens")
+]);
+
+config.watchFolders = config.watchFolders.filter((folder) => mobileWatchFolders.has(folder));
 
 module.exports = config;
